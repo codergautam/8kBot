d13b03859fe9699242d37210f5d3476c82d97711
codergautam <66850133+codergautam@users.noreply.github.com>
2020-10-23 16:22:05 -0500

    v1.4.9e translate command

---
+++ commands/translate.js
@@ -0,0 +1,28 @@
+
+const Discord = require('discord.js');
+const translate = require('translation-google');
+const ISO6391 = require('iso-639-1');
+
+module.exports = {
+    name: 'translate',
+    execute(message, args) {
+        
+        var language = (ISO6391.getCode(args[0]) == ""?args[0] : ISO6391.getCode(args[0]) )
+        console.log(language)
+        var text = args
+        text.shift()
+        text = text.join(' ')
+        translate(text, {to: language}).then(res => {
+       
+            const embed = new Discord.MessageEmbed()
+            .setDescription(res.text)
+            .setFooter(`Translated from ${ISO6391.getNativeName(res.from.language.iso)} to ${(ISO6391.getNativeName(language) == ISO6391.getName(language) ? ISO6391.getName(language): `${ISO6391.getNativeName(language)}(${ISO6391.getName(language)})`)}`)
+            message.channel.send(embed)
+            //=> en
+        }).catch(err => {
+            const embed = new Discord.MessageEmbed()
+            .setDescription(`\`\`\`${err.toString()}\`\`\`\nPlease use the command like \`8k!translate <language> <text>\``)
+            message.channel.send(embed)
+        });
+    }
+}

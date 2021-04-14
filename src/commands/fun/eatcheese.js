e0683169ba2315da867b315435c0c44048b6fc20
Coder Gautam <gautamgxtv@gmail.com>
2021-04-14 11:25:28 -0500

    Cheese? Dedicated to pokesci

---
+++ src/commands/fun/eatcheese.js
@@ -0,0 +1,14 @@
+const simpleCommand = require("../../core/simpleCommand")
+
+module.exports = new simpleCommand(
+    async(message, args, client, addCD) => {
+        message.channel.send(message.author.username + " ate some cheese!")
+    }, {
+        name: "eatcheese",
+        aliases: ["eatcheese", "eetcheese", "eatchez"],
+        cooldown: 0,
+        cooldownMessage: "",
+        usage: "{prefix}{cmd}",
+        description: "Cheese? Dedicated to pokesci"
+    }
+)

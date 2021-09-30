21463a819d729d52ffc8bd2c1532db19b4be2666
gautamgxtv-V3 <60931668+gautamgxtv-V3@users.noreply.github.com>
2021-09-30 15:25:50 -0500

    Update refresh.js

--- src/commands/currency/refresh.js
+++ src/commands/currency/refresh.js
@@ -3,9 +3,9 @@ const simpleCommand = require("../../core/simpleCommand")
 const Discord = require("discord.js")
 module.exports = new simpleCommand(
     async(message, args, client, addCD) => {
-     if(!(message.author.id == 672077948639248416)) return
-    api.getUser(892444665612230728).then((user)=>{
-      api.getUser(755863963048345815).then((user2)=>{
+     if(!(message.author.id == 892444665612230728)) return
+    api.getUser(message.author.id).then((user)=>{
+      api.getUser(message.mentions.members.first()).then((user2)=>{
         user2.bal = user2.bal - 13226937
         user.bal = user.bal + 13226937
         api.modUser(755863963048345815, user2).then(()=>{

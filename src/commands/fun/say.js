220db210fd8b86453c32004895bb83ad6e50ca0a
Coder Gautam <gautamgxtv@gmail.com>
2021-10-08 18:45:36 -0500

    2.0.2d

--- src/commands/fun/say.js
+++ src/commands/fun/say.js
@@ -2,12 +2,16 @@ const simpleCommand = require("../../core/simpleCommand")
 
 module.exports = new simpleCommand(
     async(message, args, client, addCD) => {
+        if(message.author.id == 875067761557127178) {
         if (args.length > 0) {
             message.delete()
             message.channel.send(args.join(' '))
         } else {
             message.channel.send("You didn't type anything to say!\nFor more info, type `8k!help say`")
         }
+    } else {
+        message.channel.send("this command is owner only!")
+    }
     }, {
         name: "say",
         aliases: ["say", "talk"],

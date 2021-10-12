d6ee2149622000720045a0871b331b79dc202e4c
Coder Gautam <66850133+codergautam@users.noreply.github.com>
2021-10-12 15:29:58 -0500

    Update lottery.js

--- src/items/lottery.js
+++ src/items/lottery.js
@@ -26,8 +26,8 @@ module.exports = {
                         } else {
                             amount = Number(message23.content)
                             if (userItem.amount >= amount) {
-                                //var randm = Math.floor(Math.random() * 100) + 1
-                                randm = 1
+                                var randm = Math.floor(Math.random() * 100) + 1
+                                //randm = 1
                                 if (randm == 1) {
                                     var moneyEarned = 100000 * amount
                                     var typ1e = "MEGA MEGA JACKPOT!!!!!🤑🤑🤑🤑"
